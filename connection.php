91411a34787344231bc7952bab59342d4838b6f1
Jordi Mauricio <114832904+MrCoffe2k@users.noreply.github.com>
2024-05-30 15:27:08 -0600

    Fix database host

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
  * using mysqli_connect instead
  */
 
-$databaseHost = 'rud-users-cloud.mysql.database.azure.com';
+$databaseHost = 'crud-users-cloud.mysql.database.azure.com';
 $databaseName = 'crud_with_login';
 $databaseUsername = 'U19052194';
 $databasePassword = 'CloudComputing2024!';
